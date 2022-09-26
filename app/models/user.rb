9631c6d53114218b71f7c37f2e7140c617655c79
FranciscoLunaES <francisco.luna@enroutesystems.com>
2022-09-26 10:31:09 -0700

    Add details fields for user
    - Fix her model bug
    - Add authorized persona model configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  include AuthorizedPersona::Persona
+
+  authorization_tiers(
+    member: 'Member - limited access',
+    manager: 'Manager - board access',
+    admin: 'Admin - all access'
+  )
+
+  validates :authorization_tier, inclusion: { in: authorization_tier_names }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
