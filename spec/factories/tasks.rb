9942c747cb9676998fe58a2e02cc3b391d0c427e
FranciscoLunaES <francisco.luna@enroutesystems.com>
2022-09-28 17:17:16 -0700

    Create test for models
    - Add factories for each model
    - Configure factory bot and faker

---
+++ spec/factories/tasks.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :task do
+    title { 'Fix login page' }
+    details { 'Right now' }
+    doing_time { 4 }
+    justification { 'Has a bug' }
+    started_at { '2022-09-26 23:10:19' }
+    finished_at { '2022-09-26 23:10:19' }
+    list_id { '' }
+  end
+end
---
+++ spec/models/list_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe List, type: :model do
+  before(:all) do
+    @user = create(:user)
+    @board = create(:board, user_id: @user.id)
+    @list = build(:list, board_id: @board.id)
+  end
+
+  it 'creates a list if it is valid' do
+    expect(@list).to be_valid
+  end
+
+  it "doesn't creates a board if visibility is not public or private" do
+    @list.priority = 'Not at all low'
+    expect(@list).not_to be_valid
+  end
+end
